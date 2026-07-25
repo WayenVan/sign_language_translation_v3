50e5808ee73729e15dc879775055a132d0f93065
Jingyan Wang <2533494w@login1.cognition.gla.alces.network>
2026-07-26 00:19:32 +0100

    okay

--- run_sbatch_qwen.sh
+++ run_sbatch_qwen.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=outputs/logs/%x_%j.out
 #SBATCH --error=outputs/logs/%x_%j.err
 #SBATCH --partition=gpu-h100
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=256g
 
@@ -32,12 +32,12 @@ DATASET_PATH=$(prepare_dataset \
   "$HOME/localscratch/ph14t")
 echo "DATASET_PATH=$DATASET_PATH"
 
-accelerate launch --num_processes=3 --mixed_precision=bf16 --debug -m csi_slt.commands.train \
+accelerate launch --num_processes=2 --mixed_precision=bf16 --debug -m csi_slt.commands.train \
   model=qwen3-8b-dino-b-dinoframe \
   engine.training_args.output_dir=outputs/qwen3-8b-dino-b-dinoframe \
-  engine.training_args.per_device_train_batch_size=1 \
+  engine.training_args.per_device_train_batch_size=2 \
   engine.training_args.per_device_eval_batch_size=1 \
-  engine.training_args.dataloader_num_workers=2 \
+  engine.training_args.dataloader_num_workers=4 \
   engine.training_args.eval_steps=4000 \
   engine.training_args.save_steps=4000 \
   engine.training_args.logging_steps=15 \
